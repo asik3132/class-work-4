ab6b9ded363cabfde7a675a4eb33068f811f50b0
asik <admin@DESKTOP-9K3DNSN.ilim.local>
2023-03-02 22:28:43 +0600

    first commit

---
+++ bin/second_project.dart
@@ -0,0 +1,12 @@
+import 'dart:ffi';
+
+void main() {
+  List a = ['I', 'Started', 'Learn', 'Flutter', 'Since', 'April'];
+  String myFlutter = '';
+  print(a.join('*'));
+  print(myFlutter);
+
+  List b = [1, 9, 3, 195, 202, 2, 5, 7, 9, 10, 3, 15, 0, 11];
+  b.sort();
+  print(b);
+}
